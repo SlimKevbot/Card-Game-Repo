f3a3457320dae54785c9db900927113f670fbaea
SlimKevbot <kevin.superkbc@gmail.com>
2024-01-09 14:56:33 -0600

    Cleanup

--- Main Card Reader.py
+++ Main Card Reader.py
@@ -5,14 +5,14 @@ import csv
 import os
 import textwrap
 import shutil
-from PIL import Image, ImageDraw, ImageFont, ImageOps
+from PIL import Image, ImageDraw, ImageFont
 
 def main():
-    #do something
+    #initialized
     csv_path = 'input_card_table.csv'
     blankCardPath = 'mtgcard.jpg'
     output_folder = 'deck1'
-    num_cards = 3
+    num_cards = 69
     if os.path.exists(output_folder):
         # Folder exists, empty its contents
         for file_name in os.listdir(output_folder):
@@ -28,19 +28,19 @@ def main():
         # Folder doesn't exist, create it
         os.makedirs(output_folder)
 
-    with open(csv_path) as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        line_count = 0
-        for row in csv_reader:
-            if line_count ==0:
-                print(f'Column names are {",".join(row)}')
-                line_count += 1 #this ensures that the first row is named so that we understand what is going on
-            else:
-                print(f'card {line_count} {",". join(row)} ')
-                line_count += 1
-        print(f'Processesd {line_count} lines.')
+    #with open(csv_path) as csv_file:
+    #    csv_reader = csv.reader(csv_file, delimiter=',')
+    #    line_count = 0
+    #    for row in csv_reader:
+    #        if line_count ==0:
+    #            print(f'Column names are {",".join(row)}')
+    #            line_count += 1 #this ensures that the first row is named so that we understand what is going on
+    #        else:
+    #            print(f'card {line_count} {",". join(row)} ')
+    #            line_count += 1
+    #    print(f'Processesd {line_count} lines.')
 
-    generate_cards(csv_path, blankCardPath, output_folder, num_cards)
+    generate_cards(csv_path, blankCardPath, output_folder, num_cards) #call generate card function
 
 
 
@@ -49,25 +49,23 @@ def create_card_image(trueName, tags, cardType, rules, mana, flavorText, blankCa
     blankCard = Image.open(blankCardPath).convert("RGBA") #allows transparency
     #get a drawing context
     draw = ImageDraw.Draw(blankCard)
-    #load font
+    #load fonts
     regFont = ImageFont.truetype("arial.ttf", 30)
     boldFont = ImageFont.truetype("arialbd.ttf", 30)
     italFont = ImageFont.truetype("ariali.ttf", 30)
     #define the text position
-    trueNamePos = (65,60)
-    #tagsPos = (70,590) #the card school type
+    trueNamePos = (65,60)#name of card
     cardTypePos = (70,590)#if spell or modifier
-    rulesPos = (70, 650)
+    rulesPos = (70, 650)#what card does
     wrappedRules = textwrap.fill(rules, width=35)  # Adjust the width as needed
-    flavorTextPos = (70, 800)
+    flavorTextPos = (70, 800)#in universe roleplay
     wrappedFlavor = textwrap.fill(flavorText, width=35)
-    manaPos = (590, 925)
+    manaPos = (590, 925)#mana cost
     #draw the text on the image
     draw.text(trueNamePos, f"Name: {trueName}", font=regFont, fill="red")
     draw.text(rulesPos, f"rules: {wrappedRules}", font=regFont, fill="red")
     draw.text(flavorTextPos, f"Flavor: {wrappedFlavor}", font=italFont, fill="red")
     draw.text(manaPos, f"{mana}", font=boldFont, fill="red")
-    #draw.text(tagsPos, f"School: {tags}", font=italFont, fill="red")
     draw.text(cardTypePos, f"Type: {cardType}", font=italFont, fill="red")
     
     #draw the Sin Icon on the card
@@ -79,17 +77,16 @@ def create_card_image(trueName, tags, cardType, rules, mana, flavorText, blankCa
         max_height = blankCard.height - 2 * 350
         sin_image.thumbnail((max_width, max_height))
 
-        # Create a mask from the suit image's alpha channel
-        #mask = ImageOps.extrude(sin_image.split()[3], border=1, fill=0)
+        # use the sin image as a mask to retain transparency
         blankCard.paste(sin_image, (625, 62), sin_image)  # Adjust the position as needed
     
-    # Check if the file already exists, add a counter to the filename if needed
+    # Check if the card image already exists, add a counter to the filename if needed
     counter = 1
     while os.path.exists(outputPath):
         outputPath = f"{output_folder}/{trueName.replace(' ', '_')}_{counter}_card.png"
         counter += 1
 
-    #save the card with the general name as the image name
+    #save the card with the specific card name as the image name
     blankCard.save(outputPath)
 
 def generate_cards(csv_path, blankCardPath, output_folder, num_cards):
@@ -99,16 +96,13 @@ def generate_cards(csv_path, blankCardPath, output_folder, num_cards):
         for i, row in enumerate(reader):
             if i >= num_cards:
                 break
-            #generalName = row['\ufeffGeneral Name']
             trueName = row['Specific Card Name']
             tags = row['Tags']
             rules = row['Rules Text']
             cardType = row['Card Type']
-            #card_func = row['Card Functionality Type']
             mana = row['Mana Cost']
             flavorText = row['Flavor Text']
             #generate the unique file name for each card image
-            #print(f"#{i} card generating.")
             outputPath = f"{output_folder}/{trueName.replace(' ', '_')}_card.png"
             #call the create a card function
             create_card_image(trueName, tags, cardType, rules, mana, flavorText, blankCardPath, outputPath, output_folder)
